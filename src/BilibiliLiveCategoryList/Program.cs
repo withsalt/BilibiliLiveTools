b565a927a7605c7eed12af4ed66e82509972a79c
withsalt <sysrun@163.com>
2020-08-09 21:17:57 +0800

    修改直播分区获取工具名称

--- src/BilibiliLiveCategoryList/Program.cs
+++ src/BilibiliLiveCategoryList/Program.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BilibiliLiveCategoryList
+namespace BilibiliLiveCategoryTools
 {
     class Program
     {
