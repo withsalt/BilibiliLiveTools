cf470eb306d2e793b9d65e21aba3cbdedbbe7a78
WithSalt <sysrun@163.com>
2024-07-02 21:33:14 +0800

    fix: update

---
+++ src/BilibiliAutoLiver/Models/Dtos/PushSettingDto.cs
@@ -0,0 +1,42 @@
+﻿using BilibiliAutoLiver.Models.Enums;
+
+namespace BilibiliAutoLiver.Models.Dtos
+{
+    public class PushSettingDto
+    {
+        /// <summary>
+        /// 输入类型
+        /// </summary>
+        public InputType InputType { get; set; }
+
+        /// <summary>
+        /// 输出分辨率（宽）
+        /// </summary>
+        public int OutputWidth { get; set; }
+
+        /// <summary>
+        /// 输出分辨率（高）
+        /// </summary>
+        public int OutputHeight { get; set; }
+
+        /// <summary>
+        /// 自定义输出参数
+        /// </summary>
+        public string CustumOutputParams { get; set; }
+
+        /// <summary>
+        /// 推流视频Id
+        /// </summary>
+        public string VideoPath { get; set; }
+
+        /// <summary>
+        /// 推流音频Id
+        /// </summary>
+        public string AudioPath { get; set; }
+
+        /// <summary>
+        /// 是否静音
+        /// </summary>
+        public bool IsMute { get; set; }
+    }
+}
